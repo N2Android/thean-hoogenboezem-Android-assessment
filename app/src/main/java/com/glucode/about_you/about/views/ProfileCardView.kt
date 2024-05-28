656097a9d2718faab86e875b8036f6fa3e221580
Thean Hoogenboezem <thean88@gmail.com>
2024-05-28 09:39:20 +0200

    Feature: Engineer ProfileCard * Moved styling to the XML for matching design on re-usability * Changed ImageView to ShapeableImageView to Apply Round Corner to profile picture image * TODO - Image Click

--- app/src/main/java/com/glucode/about_you/about/views/ProfileCardView.kt
+++ app/src/main/java/com/glucode/about_you/about/views/ProfileCardView.kt
@@ -5,7 +5,6 @@ import android.util.AttributeSet
 import android.view.LayoutInflater
 import android.widget.TextView
 import androidx.cardview.widget.CardView
-import androidx.core.content.ContextCompat
 import com.glucode.about_you.R
 import com.glucode.about_you.databinding.ViewEngineerProfileCardBinding
 
@@ -16,18 +15,13 @@ class ProfileCardView @JvmOverloads constructor(
         ViewEngineerProfileCardBinding.inflate(LayoutInflater.from(context),
             this)
 
-    //TODO private val profilePicture: ImageView = binding.profileImage
+    //TODO - private val profilePicture: ImageView = binding.profileImage
     private val name: TextView = binding.name
     private val role: TextView = binding.role
 
     init {
-        val whiteColour = ContextCompat.getColor(context, R.color.white)
-        val blackColour = ContextCompat.getColor(context, R.color.black)
         radius = resources.getDimension(R.dimen.corner_radius_normal)
         elevation = resources.getDimension(R.dimen.elevation_normal)
-        name.setTextColor(whiteColour)
-        role.setTextColor(whiteColour)
-        setCardBackgroundColor(blackColour)
     }
 
     fun addName(engineerName: String){
